9882b2a5bc1afd716b4720ad97f871772648a190
farizardin <fariz.ardin@gmail.com>
2019-12-16 12:11:51 +0700

    update project

--- Assets/Script/EventTrigger.cs
+++ Assets/Script/EventTrigger.cs
@@ -14,14 +14,10 @@ public class EventTrigger : MonoBehaviour
     private int hp = 3;
     private int point = 0;
     private int gameScore;
-    public Canvas panel;
-    //public Panel skorFinal;
-    // Start is called before the first frame update
     void Start()
     {
         healt.GetComponent<TMPro.TextMeshProUGUI>().text = "3";
         score.GetComponent<TMPro.TextMeshProUGUI>().text = "0";
-        //panel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,7 +33,7 @@ public class EventTrigger : MonoBehaviour
         }
         if (other.name == "Laser")
         {
-            if(hp >0)
+            if(hp > 0)
             {
                 hp -= 1;
                 Debug.Log(hp);
@@ -45,15 +41,12 @@ public class EventTrigger : MonoBehaviour
                 AsteroidGenerator setLokasiBaru = new AsteroidGenerator();
                 setLokasiBaru.LocationTrigger(asteroid, asteroidLokasi);
             }
-            else if (hp<1)
+            if (hp <= 0)
             {
-                //panel.gameObject.SetActive(true);
                 hp = 0;
+                Debug.Log(hp);
                 healt.GetComponent<TMPro.TextMeshProUGUI>().text = hp.ToString();
-                //Time.timeScale = 0;
                 gameScore = point;
-                //endScore.GetComponent<TMPro.TextMeshProUGUI>().text = gameScore.ToString();
-                //skorFinal.skorAkhir = gameScore;
                 Panel.Skor(gameScore);
                 CamTexture.OnCameraStop();
                 SceneManager.LoadScene("GameOver");
